c2001edc0d54c46bb982edc30d5dcd8de5bcaac8
ccoones <lordbaine@hotmail.com>
2013-04-29 13:15:54 -0600

    Oops

--- JQueryJEditableBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace JQuery\JEditableBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class JQueryJEditableBundle extends Bundle
-{
-}
---
+++ TuupolaJEditableBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tuupola\JEditableBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TuupolaJEditableBundle extends Bundle
+{
+}
